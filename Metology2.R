f85e142b0af6e46bc1865103e35241010cb9a20f
hebermudezg <hebermudezg@unal.edu.co>
2019-06-30 18:53:48 -0500

    utlimos cambios metodology 2

--- Metology2.R
+++ Metology2.R
@@ -1,6 +1,7 @@
 
 # librerias
 library(tm)
+library(wordcloud)
 
 
 # cargando documentos recuperados mediante web scraping.
@@ -47,7 +48,7 @@ Corpus_term <- tm_map(Corpus_term, tolower)
 Corpus_term <- tm_map(Corpus_term, removePunctuation)
 
 # removiendo stopwors
-#Corpus_term <- tm_map(Corpus_term, removeWords, stopwords("spanish"))
+Corpus_term <- tm_map(Corpus_term, removeWords, stopwords("spanish"))
 
 
 # removiendo otras palabras
@@ -58,9 +59,13 @@ Corpus_term <- tm_map(Corpus_term, removeWords, otras_palabras)
 # removiendo numeros
 Corpus_term <- tm_map(Corpus_term, removeNumbers)
 
+# removiendo espacios en blanco 
+Corpus_term <- tm_map(Corpus_term, stripWhitespace)
+
+
 
 # remover mediante expresiones regulares. 
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+tm_map(Corpus_term, gsub, patte ="patron", replacement =" ")
 
 
 # stemming o lematizacion (buscar en español) ********************
@@ -73,15 +78,43 @@ Corpus_frecu <- TermDocumentMatrix(Corpus_term)
 Corpus_frecu #imprime atributos de frequencies
 inspect(Corpus_frecu[1:20, 1:3]) #zoop de la matriz / posiciones [15:20, 5:10]
 
-
 Corpus_frecu_mat <- as.matrix(Corpus_frecu)
-colnames(document_tm_mat) <- unique_labels
+
+######################
+
+comparar <- as.data.frame(Corpus_frecu_mat_limpio_mat)
+names(comparar)
+comparar$total <- apply(comparar, 1, sum)
+
+
+
+
+
+colnames(Corpus_frecu_mat) <- c("Analitica_de_datos",
+                                "big_data", "ciencia_de_datos",
+                                 "estadistica", "inteligencia_artificial",
+                                 "machine_learning", "ofertas_ciencia_de_datos",
+                                 "Ofertas_estadistica")
 Corpus_frecu_mat_limpio <- removeSparseTerms(Corpus_frecu, 0.8)
 Corpus_frecu_mat_limpio_mat <- as.matrix(Corpus_frecu_mat_limpio)
-colnames(Corpus_frecu_mat_limpio_mat) <- unique_labels
+colnames(Corpus_frecu_mat_limpio_mat) <- c("Analitica_de_datos",
+                                           "big_data", "ciencia_de_datos",
+                                           "estadistica", "inteligencia_artificial",
+                                           "machine_learning", "ofertas_ciencia_de_datos",
+                                           "Ofertas_estadistica")
 
 
 
+
+
+aa <- TermDocumentMatrix(Corpus_term, list(weighting=weightTfIdf))
+Corpus_frecu_mat_limpio <- removeSparseTerms(aa, 0.8)
+Corpus_frecu_mat_limpio_mat <- as.matrix(Corpus_frecu_mat_limpio)
+colnames(Corpus_frecu_mat_limpio_mat) <- c("Analitica_de_datos",
+                                           "big_data", "ciencia_de_datos",
+                                           "estadistica", "inteligencia_artificial",
+                                           "machine_learning", "ofertas_ciencia_de_datos",
+                                           "Ofertas_estadistica")
 
 
 # -- STEP 3 : make the graphics !
@@ -89,7 +122,8 @@ colnames(Corpus_frecu_mat_limpio_mat) <- unique_labels
 # Graph 1 : first top 500 discriminant words
 #png("#102_1_comparison_cloud_top_500_words.png", width = 480, height = 480)
 win.graph()
-comparison.cloud(Corpus_frecu_mat_limpio_mat, max.words=500, random.order=FALSE,c(4,0.4), title.size=1.4)
+comparison.cloud(Corpus_frecu_mat_limpio_mat, max.words=500, random.order=FALSE,c(4,0.4), title.size=0.8, rot.per = .3,
+                 colors = brewer.pal(10, "Paired"))
 #dev.off()
 
 # Graph 2 : first top 2000 discriminant words
@@ -139,11 +173,53 @@ rowSums(as.matrix(tdm)>0)
 
 
 
+## Document Clustering with R
+
+library(tm)
+library(proxy)
+
+
+m  <- as.matrix(Corpus_frecu)
+colnames(m) <- c("Analitica_de_datos",
+                                           "big_data", "ciencia_de_datos",
+                                           "estadistica", "inteligencia_artificial",
+                                           "machine_learning", "ofertas_ciencia_de_datos",
+                                           "Ofertas_estadistica")
+distMatrix <- dist(t(m), method="euclidean")
+
+# agrupameitnto jerarquico  CLUSTER 
+
+groups <- hclust(distMatrix,method="ward.D")
+win.graph()
+plot(groups, cex=0.9, hang=-1)
+rect.hclust(groups, k=4)
 
 
 
+## ANALISI DE COMPONENTES PRINCIPALES
 
 
+
+articleDtm <- DocumentTermMatrix(Corpus_term)
+articleDtm2 <- as.matrix(articleDtm)
+
+
+inspect(articleDtm)
+articles_mat <- as.matrix(articleDtm)
+rownames(articles_mat) <- c("Analitica_de_datos",
+                            "big_data", "ciencia_de_datos",
+                            "estadistica", "inteligencia_artificial",
+                            "machine_learning", "ofertas_ciencia_de_datos",
+                            "Ofertas_estadistica")
+
+distMatrix <- dist(articles_mat, method="euclidean")
+groups <- hclust(distMatrix,method="ward.D")
+plot(groups, cex=0.9)
+rect.hclust(groups, k=3)
+
+
+library(FactoMineR)
+PCA(t(articles_mat))
 
 
 
